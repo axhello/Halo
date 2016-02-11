0b7c56534cf138950e8ce5383d56f2ac1419b985
melifes <zhanglin-pc@live.com>
2016-02-11 19:45:43 +0800

    fix

--- footer.php
+++ footer.php
@@ -3,7 +3,9 @@
             <p>Design by © Bootstrap, but please download and customize it for yourself!</p>
         </div>
     </footer>
-    <div id="topcontrol" style="position: fixed; bottom: 30px; right: -5px; opacity: 1; cursor: pointer;"><img src="<?php $this->options->themeUrl('images/backTop.png'); ?>" width="100"></div>
+    <div id="topcontrol" style="position: fixed; bottom: 30px; right: -5px; opacity: 1; cursor: pointer;z-index: 999;">
+        <img src="<?php $this->options->themeUrl('images/backTop.png'); ?>" width="100">
+    </div>
     <script src="<?php $this->options->themeUrl('js/jquery.min.js'); ?>"></script>
     <script src="<?php $this->options->themeUrl('js/bootstrap-hover-dropdown.min.js'); ?>"></script>
     <script src="<?php $this->options->themeUrl('js/bootstrap.min.js'); ?>"></script>
